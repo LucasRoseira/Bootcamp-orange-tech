485e6066d94b52265f233cb0190b393d0ece6df7
Lucas Gomes <lucas.roseir@fatec.sp.gov.br>
2022-12-12 21:59:10 -0300

    feat: início pokedex

--- JS/06 - litas(arrays)/aula.js
+++ JS/06 - litas(arrays)/aula.js
@@ -1,15 +1,38 @@
 
-const alunos = ['João', 'Vitor', 'Marina'];
-console.log(alunos[0]);
-console.log(alunos[1]);
-console.log(alunos[2]);
-console.table(alunos);
+// const alunos = ['João', 'Vitor', 'Marina'];
+// console.log(alunos[0]);
+// console.log(alunos[1]);
+// console.log(alunos[2]);
+// console.table(alunos);
 
-alunos.push('Renan');
-alunos[4] = 'Selma';
+// alunos.push('Renan');
+// alunos[4] = 'Selma';
 
-console.table(alunos);
+// console.table(alunos);
 
-alunos.pop();
-console.table(alunos);
+// alunos.pop();
+// console.table(alunos);
 
+// const nome = 'Lucas Gomes Roseira';
+
+// for (let i = 0; i < nome.length; i++) {
+//     console.log(nome[i]);
+// }
+
+const notas = [];
+let soma = 0;
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push(8);
+
+
+for (let i = 0; i < notas.length; i++) {
+    soma += notas[i];
+}
+let media = (soma / notas.length);
+
+console.log('Media: ' + media.toFixed(2));
---
+++ JS/07 - Importação/main.js
@@ -0,0 +1,44 @@
+const { gets, print } = require('./funcoes-auxiliares');
+
+
+// const funcoes {gets, print} = require('./funcoes-auxiliares');
+
+// console.log(funcoes);
+
+/*
+Uma sala contém 5 alunos e para cada aluno foi sorteado um número de 1 - 100
+Faça um programa que receba 5 números sorteados para os alunos e mostre o maior número sorteado
+
+Dados de entrada: 
+5 
+50
+10
+98
+23
+
+Saída: 98
+
+*/
+
+let maiorValorEncontrado = 0;
+const quantidadeDeAlunos = gets();
+
+for (let i = 0; i < quantidadeDeAlunos; i++) {
+
+    const numeroSorteado = gets();
+
+    if (numeroSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numeroSorteado;
+    }
+
+}
+
+print(maiorValorEncontrado);
+
+// for (let i = 0; i < numeroSorteados.length; i++) {
+//     const numeroSorteado = numeroSorteados[i];
+//     if (numeroSorteado > maiorValor) {
+//         maiorValor = numeroSorteado;
+//     }
+// }
+
