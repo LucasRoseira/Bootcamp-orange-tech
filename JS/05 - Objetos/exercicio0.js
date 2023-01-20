6087223f88379ea1234161daab2c0f84f14fca72
LucasRoseira <lucas.roseira@fatec.sp.gov.br>
2023-01-20 20:48:07 -0300

    fix

--- JS/05 - Objetos/exercicio0.js
+++ JS/05 - Objetos/exercicio0.js
@@ -2,8 +2,7 @@
 1 - Crie uma classe para representar carros.
 Os carros possuem uma marca, uma cor e um gasto médio de combustível por Kilômetro rodado.
 Crie um método que dado a quantidade de quilometros e o preço do combustível nos dê o valor
-gasto em reais para realizar este percurso.
- */
+gasto em reais para realizar este percurso.*/
 
 class Carro {
     marca;
